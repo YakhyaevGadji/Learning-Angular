ede80c91d2bb38f2110e017eaae9cf919aa203c0
gadji0550 <gadji180105@gmail.com>
2023-05-17 13:26:31 +0300

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-basics';
+  
+
+  func(event: any) {
+    this.title = event.target.value += 'hello';
+  }
+
 }
